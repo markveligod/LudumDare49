e72cfb72602b4714a46e97fd805b6b0cfd92a415
Exelution <74369712+Exelution@users.noreply.github.com>
2021-10-03 18:02:09 +0300

    added BossBall and BossPillar game is really unstable

--- Source/LudumDare49/Private/Game/BadBall/BadBallPawn.cpp
+++ Source/LudumDare49/Private/Game/BadBall/BadBallPawn.cpp
@@ -4,59 +4,54 @@
 #include "Engine/World.h"
 #include "Kismet/GameplayStatics.h"
 
-
 DEFINE_LOG_CATEGORY_STATIC(LogBadBall, All, All)
 
 // Sets default values
 ABadBallPawn::ABadBallPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	Mesh= CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
     Mesh->SetSimulatePhysics(true);
 }
 
 // Called when the game starts or when spawned
 void ABadBallPawn::BeginPlay()
 {
-	Super::BeginPlay();
-    
+    Super::BeginPlay();
+
     StartMove();
 }
 
-void ABadBallPawn::StartMove() 
+void ABadBallPawn::StartMove()
 {
     float FirstDelay = FMath::RandRange(MinFirstDelay, MaxFirstDelay);
- 
-    GetWorldTimerManager().SetTimer(MoveImpulseTimerHandle,this, &ABadBallPawn::MoveImpulse, MoveImpulseDelay, true,FirstDelay);
+
+    GetWorldTimerManager().SetTimer(MoveImpulseTimerHandle, this, &ABadBallPawn::MoveImpulse, MoveImpulseDelay, true, FirstDelay);
 }
 
 void ABadBallPawn::StopMove() {}
 
-void ABadBallPawn::MoveImpulse() 
+void ABadBallPawn::MoveImpulse()
 {
-    
+
     //////////////////// choosing forse ///////////////////////////////// choosing direction
-    float XDirection = FMath::RandRange(MinMoveImpulse, MaxMoveImpulse) * FMath::RandRange(-1,1);
+    float XDirection = FMath::RandRange(MinMoveImpulse, MaxMoveImpulse) * FMath::RandRange(-1, 1);
     float YDirection = FMath::RandRange(MinMoveImpulse, MaxMoveImpulse) * FMath::RandRange(-1, 1);
 
     FVector ImpulseDirection(XDirection, YDirection, 0.0f);
-    UE_LOG(LogBadBall, Display, TEXT("X : %f  ,  Y: %f"), XDirection, YDirection);
-    Mesh->AddImpulse(ImpulseDirection*MoveImpulseScale, NAME_None, true);
+    // UE_LOG(LogBadBall, Display, TEXT("X : %f  ,  Y: %f"), XDirection, YDirection);
+    Mesh->AddImpulse(ImpulseDirection * MoveImpulseScale, NAME_None, true);
 }
 
 // Called every frame
 void ABadBallPawn::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-    
-
+    Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
 void ABadBallPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
-
---
+++ Source/LudumDare49/Public/Game/BossBall/BossBallPawn.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SphereComponent.h"
+#include "Game/Neutron/NeutronActor.h"
+
+#include "BossBallPawn.generated.h"
+
+USphereComponent;
+ANeutronActor;
+
+UCLASS()
+class LUDUMDARE49_API ABossBallPawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this pawn's properties
+    ABossBallPawn();
+
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UStaticMeshComponent* Mesh = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    TSubclassOf<ANeutronActor> NeutronActor;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    USphereComponent* SphereComponent = nullptr;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Movement")
+    float MinDeviationImpulse = -0.30f;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Movement")
+    float MaxDeviationImpulse = 0.30f;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Movement")
+    float MoveImpulseScale = 100.0f;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Timer")
+    float MoveImpulseDelay = 1.0f;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Movement")
+    float MaxDetectDistance = 10000.0f;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Health")
+    int32 MaxHealth = 3;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Neutrons")
+    int32 AmountOfNeutronsAfterDeath = 15;
+
+    UFUNCTION()
+    void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+    void MoveImpulse();
+
+    int32 CurrentHealth = 3;
+    void Death();
+
+    FTimerHandle MoveImpulseTimerHandle;
+};
